dbab072bef92f527e88ef99c8c0230ba6513fd2a
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-05-26 10:32:46 -0500

    complete flow exercises

--- src/ControlFlow.java
+++ src/ControlFlow.java
@@ -2,135 +2,195 @@ import java.util.Scanner;
 
 public class ControlFlow {
     public static void main(String[] args) {
+        /*---------Conditional branching------------*/
 //        int x = 5;
+//
 //        if (x == 5) {
-//            System.out.println("Hey user. If you are reading this, it means x is 5");
+//            System.out.println("If you are reading this, it means x is 5");
 //        }
-//        System.out.println("On the other hans, this says nothing about x, because it runs regardless of what x is");
-        int y = 2;
-        if (y == 2) {
-            System.out.println("I've carried out a conditional test and i can confirm that the statement y==2 is " +
-                    "true");
-        } else {
-            System.out.println("I've tested to see if y==2 is true, and it is not. It is false");
-        }
-        short z = -5;
-        if (z < 0) {
-            System.out.println("The variable z contains a negative number value");
-        } else if (z >= 0 && z < 30000) {
-            System.out.println("The variable z contains a positive number value");
-        } else {
-            System.out.println("The variable z contains a numeric value thats is dangerously close to the max value " +
-                    "for short type variable, eich is: " + Short.MAX_VALUE);
-        }
+//
+//        System.out.println("on the other hand, this says nothing about x, because it runs regardless of what x is");
 
-        //Switch statement
-        String customerChoice;
-        customerChoice = "chocolate";
-        switch (customerChoice) {
-            case "vanilla":
-                System.out.println("One vanilla coming right up");
-                break;
-            case "chocolate":
-                System.out.println("chocolate coming right up");
-                break;
-            case "strawberry":
-                System.out.println("strawberry coming up");
-                break;
-            default:
-                System.out.println("we haven't that! go away!");
-                break;
-        }
+//        int y = 2;
+//        y = 3;
+//        if (y == 2) {
+//            System.out.println("I've carried out a conditional test and I can confirm that the statement 'y==2' is true");
+//        } else {
+//            System.out.println("I have tested to see if 'y==2' is true, and it's not. It is false.");
+//        }
 
-        //Operators
+//        short y = -5;
+//        y = 12;
+//        y = 32000;
+////        y = 33000; can't put an int into a short
+//        if (y < 0){
+//            System.out.println("The variable y contains a negative number value");
+//        } else if (y >=0 && y < 30000){
+//            System.out.println("The variable y contains a positive number value");
+//        } else {
+//            System.out.println("The variable y contains a numeric value that is dangerously close to the max value for short type variables, which is " + Short.MAX_VALUE);
+//        }
 
-        int c = 0, d = 100, e = 50;
-        // short circuit operator
-        if (c == 1 && e < 100) {
-            //do something
-        }
-        System.out.println("e = " + e);
-        if (c == 0 && e++ < 100) {
-            //do something
-            d = 150;
-        }
-        System.out.println("e = " + e);
-        if (c == 1 & e++ < 100) {
-            //do something
-        }
-        System.out.println("e = " + e);
+//        String customerChoice;
+//        customerChoice = "caramel pistachio mocha";
+//
+//        switch (customerChoice) {
+//            case "vanilla":
+//                System.out.println("One vanilla coming right up");
+//                break;
+//            case "chocolate":
+//                System.out.println("Chocolate coming right up");
+//                break;
+//            case "strawberry":
+//                System.out.println("Strawberry coming right up");
+//                break;
+//            default:
+//                System.out.println("We don't have that! Go away!");
+//                break;
+//        }
 
-        // booleans
-        boolean existence = true;
-        boolean solipsism = false;
-        boolean weAllLiveInSomeElsesDream = false;
-        boolean thisDeskIsRealNotAProjectionOfMyMind = false;
-        if (thisDeskIsRealNotAProjectionOfMyMind | weAllLiveInSomeElsesDream) {
-            System.out.println("common sense is victorious");
-        } else {
-            System.out.println("Im really confused now");
-        }
-        if (solipsism || weAllLiveInSomeElsesDream) {
-            System.out.println("common sense is victorious");
-        } else {
-            System.out.println("Im really confused now");
-        }
-        //comparison operator ==
-        //there is not strict equality operator
-        //inequality !=
-        //there is no strict inequality don't do this !==
+//        int customerMaxBudget = 10;
+//        // you can't do this. No boolean decisions in switch cases
+//        switch (customerMaxBudget){
+//            case (>=0 && <=1000){
+//                System.out.println("Wouldn't  you prefer a place for more limited budgets?");
+//            }
+//        }
 
+//        String existence = "I exist";
+//        boolean existence = true;
+//
+//        if (existence){
+//            System.out.println("The programmer is a real being");
+//        }
+
+        /*-------Logical Operators----------*/
+
+        // you can do && || &
+
+
+//        int c = 0, d = 100, e = 50;
+//        // short circuit operator
+//        if (c == 1 && e++ < 100){
+//            // do anything
+//        }
+//        System.out.println("e = " + e);
+//
+//        if (c == 0 && e++ < 100){
+//            // do anything
+//            d = 150;
+//        }
+//        System.out.println("e = " + e);
+//
+//        // non short circuit operator
+//        if (c == 1 & e++ < 100){
+//            // anything goes here
+//        }
+//        System.out.println("e = " + e);
+
+//        boolean existence = true;
+//        boolean solipsism = false;
+//        boolean weAllLiveInSomeoneElsesDream = false;
+//        boolean thisDeskIsRealNotAProjectionOfMyMind = true;
+//
+//        if (solipsism | thisDeskIsRealNotAProjectionOfMyMind) {
+//            System.out.println("Common sense is victorious");
+//        } else {
+//            System.out.println("I'm really confused now");
+//        }
+//
+//        if (solipsism || weAllLiveInSomeoneElsesDream) {
+//            System.out.println("Common sense is victorious");
+//        } else {
+//            System.out.println("I'm really confused now");
+//        }
+
+        // comparison operator ==
+        // there is no strict equality operator
+        // inequality operator !=
+        // there is no strict inequality don't do this: !==
+
+        // these are stored in the string pool
         String myName = "Javier";
         String anotherJavier = "Javier";
+
         String javier = new String("Javier");
 
-        System.out.println(myName == anotherJavier);
-        System.out.println(myName == javier);
+//       System.out.println(myName == anotherJavier);
+//       System.out.println(myName == javier);
 
-//Scanner scanner = newScanner(System.in) Whit this you can interactive with the keyboard
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("what is your name?");
-        String userInput = scanner.next();
-//        if (userInput == myName) {
-//            System.out.println("Hello, me");
+//       Scanner scanner = new Scanner(System.in);
+//        System.out.println("What is your name?");
+//        String userInput = scanner.next();
+//        if (userInput == myName){
+//            System.out.println("Hello, me!");
 //        } else {
 //            System.out.println("You are not me");
 //        }
-        //Use .equals
-        if (userInput.equals(myName)) {
-            System.out.println("Hello, me");
-        } else {
-            System.out.println("You are not me");
-        }
-//        Loops
+
+//        if (userInput.equals(myName)){
+//            System.out.println("Hello, me!");
+//        } else {
+//            System.out.println("You are not me");
+//        }
+
+        /*-------------Loops--------------------*/
 
 //        int i = 1;
 //        while (i < 10) {
-//            System.out.println("Now I'm in the Loop. I has not incremented yet and its value is: " + i);
-//            // i++ is short hand for i = i + 1
+//            System.out.println("Now I'm in the loop. i has not incremented yet and its value is " + i);
+//            // i++ is shorthand for i = i + 1;
 //            i++;
-//            System.out.println("I'm still in the Loop, but i has incremented now, and its value is: " + i);
+//            System.out.println("I'm still in the loop, but i has incremented now, and its value is " + i);
 //        }
-//        System.out.println("This is outside the Loop, after it has executed. The conditional has tested false. The " +
-//                "value of i is now: " + i);
+//        System.out.println("This is outside the loop, after it has executed. The conditional has tested false. The value of i is now " + i);
+//
+//        i = 15427;
+//
+//        do {
+//            System.out.println("Now I'm in the loop. i has not incremented yet and its value is " + i);
+//            // i++ is shorthand for i = i + 1;
+//            i++;
+//            System.out.println("I'm still in the loop, but i has incremented now, and its value is " + i);
+//        } while (i < 10);
+//        System.out.println("This is outside the loop, after it has executed. The conditional has tested false. The value of i is now " + i);
+//    }
 
 //        for (int i = 0; i < 10; i++) {
-//            System.out.println("Now I'm in the Loop. I has not incremented yet and its value is: " + i);
+//            System.out.println(/*"Now I'm in the loop. i has not incremented yet and its value is " +*/ i);
 //        }
-        for (int i = 100; i > 0; i--) {
-            String word = "cans";
-            if (i == 1) {
-                System.out.println(i + " can of Big Red on the wall");
-                System.out.println(i + " can of Big Red");
-            } else if (i == 0) {
-                System.out.println("No more cans of big red on the wall");
-            } else {
-                System.out.println(i + " cans of Big Red on the wall");
-                System.out.println(i + " cans of Big Red");
-                System.out.println("Take one down, pass it around ");
-            }
 
+        // I can't access the variable i outside the loop code block
+//        System.out.println("This is outside the loop, after it has executed. The conditional has tested false. The value of i is now " + i);
+
+        // In a for loop, the increment is always the last thing that happens in the code block. It's automatic behavior. If I want to do anything after the increment, I must use a while-style loop
+
+        // group code 99 cans of big red
+
+        for (int i = 99; i >= 0; i--) {
+            String word = "cans";
+            String phrase = " of Big Red on the Wall";
+            String shortPhrase = " of Big Red";
+            String chorusEnd = "Take one down, pass it around, ";
+            String period = ".";
+            String comma = ",";
+            if (i == 1) {
+                word = "can";
+                System.out.println(i + word + phrase + period);
+                System.out.println(i + word + phrase + comma);
+                System.out.println(i + shortPhrase + comma);
+                System.out.println(chorusEnd);
+            } else if (i == 0) {
+                System.out.println("No more " + word + phrase + period);
+                System.out.println("No more " + word + shortPhrase + comma);
+                System.out.println("You go to the store, and buy some more,");
+                System.out.println("99 " + word + phrase + period);
+            } else {
+                System.out.println(i + word + phrase + period);
+                System.out.println(i + word + phrase + comma);
+                System.out.println(i + word + shortPhrase + comma);
+                System.out.println(chorusEnd);
+            }
         }
     }
 }
--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,55 +2,115 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        //5 to 15
+        // 1. Loop Basics a. While
 //        int i = 5;
-//        while (i <= 15) {
-//            System.out.print(" " + i);
+//        while (i <= 15){
+//            if (i < 15) {
+//                System.out.print(i + " ");
+//            } else {
+//                System.out.println(i);
+//            }
 //            i++;
 //        }
-        //2 to 100
+
+        // b. Do While
 //        int i = 0;
 //        do {
 //            System.out.println(i);
-//            i +=2;
-//        } while (i < 10);
-        //100 to -10
-//        int i = 5;
+//            i += 2;
+//        } while (i <= 100);
+
+//        int i = 100;
 //        do {
 //            System.out.println(i);
 //            i -= 5;
 //        } while (i >= -10);
-        //2 to 1.000.000
-//        int i = 2;
+
+//        long i = 2L;
 //        do {
 //            System.out.println(i);
 //            i *= i;
-//        } while (i <= 256);
-        //for loop refactor last exercises
-//        for (int i = 5; i <= 15; i++) {
-//            System.out.print(" " + i);
-//        }
-//        for (int i = 2; i <= 256; i *= i) {
+//        } while (i < 1000000);
+
+        // c. For
+
+//        for (int i = 100; i >= -10; i -=5 ){
 //            System.out.println(i);
 //        }
-        //FizzBuzz test
-//        int num = 100;
-//        for (int i = 1; i <= 100; i++) {
-//            if (i % 3 == 0 && i % 5 == 0) {
-//                System.out.println("FizzBuzz");
-//            } else if (i % 3 == 0) {
-//                System.out.println("Fizz");
-//            } else if (i % 5 == 0) {
-//                System.out.println("Buzz");
-//            } else {
-//                System.out.println(" " + i);
-//            }
+
+//        for (long i = 2L; i < 1000000; i *= i){
+//            System.out.println(i);
 //        }
-        // power table
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Give me a number");
-        int userNum = scanner.nextInt();
-        System.out.println("your number was: " + userNum);
+
+        // 2. The PIN Problem
+
+//        Scanner scanner1 = new Scanner(System.in);
+//        final int PIN = 12345;
+//        int userInput = 0;
+//        int attempts = 1;
+//        while (attempts <=3){
+//            System.out.println("Please enter pin: ");
+//            userInput = scanner1.nextInt();
+//            if (userInput != PIN){
+//                if (attempts < 3){
+//                    System.out.println("Incorrect, try again");
+//                } else {
+//                    System.out.println("Sorry but you have been locked out.");
+//                }
+//            } else {
+//                System.out.println("Correct, welcome back");
+//                break;
+//            }
+//            attempts++;
+//        }
+
+        // 3. Display a table of powers
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("What number would you like to go up to?");
+//        boolean userWantsToContinue = false;
+//        do {
+//            int userInput = scanner.nextInt();
+//            System.out.println("Here is your table!");
+//            System.out.println("");
+//            System.out.println("number | squared | cubed");
+//            System.out.println("------ | ------- | -----");
+//            for (int i = 1; i <= userInput; i++) {
+//                System.out.printf("%-7d| %-8d| %-5d%n", i, i * i, i * i * i);
+//            }
+//            System.out.println("Enter another number? y/n");
+//            String userAnswer = scanner.next();
+//            if (userAnswer.equals("y")){
+//                userWantsToContinue = true;
+//            } else {
+//                break;
+//            }
+//        } while (userWantsToContinue);
+
+
+        // 4. Convert given number grades into letter grades
+
+        Scanner scanner1 = new Scanner(System.in);
+        String userChoice = "y";
+        while (!userChoice.equals("n")) {
+            System.out.println("Enter the student's numerical grade: ");
+            byte grade = scanner1.nextByte();
+            char letterGrade;
+            if (grade > 0 && grade <= 59) {
+                letterGrade = 'F';
+            } else if (grade > 59 && grade <= 66) {
+                letterGrade = 'D';
+            } else if (grade > 66 && grade <= 79) {
+                letterGrade = 'C';
+            } else if (grade > 79 && grade <= 87) {
+                letterGrade = 'B';
+            } else {
+                letterGrade = 'A';
+            }
+            System.out.println("The letter grade is: " + letterGrade);
+            System.out.println("Would you like to enter another grade?(y/n)");
+            userChoice = scanner1.next();
+        }
 
     }
 }
