17f9e820eb7a01dc1dcae67fb6228989f439f95c
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-05-29 18:30:29 -0500

    continue java

---
+++ src/MethodLecture.java
@@ -0,0 +1,38 @@
+public class MethodLecture {
+    //write functions here
+
+    public static void sayHello() {
+        System.out.println("Sorry i'm rush. Have a great day!");
+    }
+
+    public static void sayHello(String name) {
+        System.out.printf("Hi there! My name is %s! Have a great day!", name);
+    }
+
+    public static void sayHello(String salutation, String name) {
+        System.out.printf("%s Hi there! My name is %s! Have a great day!", salutation, name);
+    }
+
+    public static void sayHello(int times) {
+        for (int i = 1; i <= times; i++) {
+            System.out.println("Hello darling");
+        }
+    }
+
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+
+    public static void main(String[] args) {
+        // call the function here
+
+        sayHello("Carlos");
+        System.out.println(multiply(5, 2));
+        int product = multiply(7, 3);
+        System.out.printf("Did you know that %d x %d = 's %d \n", product, product, product * product);
+        sayHello("Charlotte", "Carlos \n");
+        sayHello();
+        sayHello(4);
+    }
+}
---
+++ src/MethodsExercises.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MethodsExercises {
+    //1.-
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public static int division(int a, int b) {
+        return a / b;
+    }
+
+    //prima fot loop TODO
+
+
+    public static void main(String[] args) {
+        System.out.println(addition(5, 7));
+        System.out.println(subtraction(4, 6));
+        System.out.println(multiplication(3, 7));
+        System.out.println(division(12, 3));
+    }
+}
