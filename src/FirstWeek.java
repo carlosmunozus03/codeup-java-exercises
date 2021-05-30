d534423e9b3d57c444e2980305d7adb620485b8c
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-05-30 15:41:59 -0500

    practice java 1 week

---
+++ src/FirstWeek.java
@@ -0,0 +1,111 @@
+public class FirstWeek {
+    public static void main(String[] args) {
+        //Hello Word
+        System.out.println("Hello world from the new line");
+        System.out.print("Hello world from a line \n");
+
+        //Syntax, Type of Data and Variables
+
+        //Primitive  8 DATA
+
+        /* === Numbers
+        byte
+        short
+        int
+        long
+         */
+
+        /* === Decimals
+        float
+        double
+         */
+
+         /* === Others
+        char
+        boolean
+         */
+
+        //variables
+        int id;
+        boolean isLoggedIn;
+        float bankAccountBalance;
+
+        id = 123;
+        isLoggedIn = false;
+        bankAccountBalance = 1000.23F;
+
+        int theAnswer;
+        String question;
+
+        theAnswer = 42;
+        question = "What is your favorite number?";
+
+        // or
+
+        int theAnswer2 = 42;
+        String question2 = "What is your favorite number?";
+
+        //Constants
+
+        final int SIDES_OF_A_DICE = 6;
+
+        //Math Operators
+
+        /* === Binary Operators
+
+        +
+        -
+        *
+        /
+        %
+
+         */
+
+         /* === Urinary Operators
+
+        ++
+        --
+        +
+        -
+
+         */
+
+        int myNumber = 12 + 12 * 2;
+
+        // Casting
+
+//value of a less precise data type to a variable whose types is of a higher precision
+
+        int myInteger = 900;
+        long morePrecise = myInteger;
+
+//from a higher precision type to a lower precision type
+        //This is also called a narrowing conversion.
+
+        double pi = 3.14159;
+        int almostPi = (int) pi;
+
+        //Exercises
+
+        int myFavoriteNumber = 42;
+        String myString = "Your Favorite number is: " + myNumber;
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
+
+        int a = 4;
+        a += 5;
+        System.out.println(a);
+
+        int b = 3;
+        int c = 4;
+        b *= c;
+        System.out.println(b);
+
+        int d = 10;
+        int e = 2;
+        d /= e;
+        e -= d;
+        System.out.println(e);
+
+    }
+}
