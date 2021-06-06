7d83ffc31c53e6cd22812b3fed5505154effcfc1
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-06-06 08:27:57 -0500

    string exercises

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,16 +1,29 @@
-
+//
+//public class StringExercise {
+//    public static void main(String[] args) {
+//        String education = "We don't need no education\nWe don't need no thought control";
+//        System.out.println(education);
+//        System.out.println("/////////");
+//        String quotations = "Check \"this\" out!, \"s inside of \"s";
+//        System.out.println(quotations);
+//        System.out.println("/////////");
+//        String driveString = "In windows, the main drive is usually C:\\";
+//        System.out.println(driveString);
+//        System.out.println("/////////");
+//        String tooMuchFunWithEscapingCharacters = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+//        System.out.println(tooMuchFunWithEscapingCharacters);
+//    }
+//}
 public class StringExercise {
     public static void main(String[] args) {
         String education = "We don't need no education\nWe don't need no thought control";
         System.out.println(education);
-        System.out.println("/////////");
-        String quotations = "Check \"this\" out!, \"s inside of \"s";
-        System.out.println(quotations);
-        System.out.println("/////////");
-        String driveString = "In windows, the main drive is usually C:\\";
-        System.out.println(driveString);
-        System.out.println("/////////");
-        String tooMuchFunWithEscapingCharacters = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
-        System.out.println(tooMuchFunWithEscapingCharacters);
+        String quotation = "\nCheck \"this\" out!, \"s inside of \"!";
+        System.out.println(quotation);
+        String windows = "\nIn windows, the main driver is usually C:\\";
+        System.out.println(windows);
+        String slashes = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslashes " +
+                "\\\\\\!";
+        System.out.println(slashes);
     }
 }
