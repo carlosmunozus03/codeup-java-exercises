13c1ae53c30c390711729e20c5ca315ea677acda
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-06-10 11:38:43 -0500

    lecture update IO

--- src/FileIOLecture.java
+++ src/FileIOLecture.java
@@ -1,11 +1,23 @@
-
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class FileIOLecture {
+
+    public void readFileAndOuput(Path pathToFile) {
+        List<String> currentList = new ArrayList<>();
+        try {
+            currentList = Files.readAllLines(pathToFile);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        for (String line : currentList) {
+            System.out.println(line);
+        }
+    }
 
     public static void main(String[] args) {
 //        Path path = Paths.get("src");
@@ -43,23 +55,98 @@ public class FileIOLecture {
 
         // Create a File
 
+//       try {
+//           if (Files.notExists(toOurDataFile)){
+//               Files.createFile(toOurDataFile);
+//           }
+//       } catch (IOException ioe) {
+//           ioe.printStackTrace();
+//       }
+//
+//       // Write to a file
+//
+//        List<String> romanEmpresses = Arrays.asList("Livia", "Agrippina", "Messaline", "Julia Domna");
+//
+//       try {
+//           // 2 arguments for Files.write
+//           // The path to our file, the data we want to write
+//           Files.write(toOurDataFile, romanEmpresses);
+//       } catch (IOException ioe){
+//           ioe.printStackTrace();
+//       }
+//
+//       // Read from a file
+//
+//       // create a place to put information we read from the file
+////       List<String> currentList = new ArrayList<>();
+////
+////       // read the information from the file
+////       try {
+////           currentList = Files.readAllLines(toOurDataFile);
+////       } catch (IOException ioe){
+////           ioe.printStackTrace();
+////       }
+////
+////       // loop through the ArrayList and output each item
+////       for (String line: currentList) {
+////           System.out.println(line);
+////       }
+
+        FileIOLecture io = new FileIOLecture();
+//        io.readFileAndOuput(toOurDataFile);
+
+        // Append a single item to our list
+
+//        try {
+//            Files.writeString(toOurDataFile, "Julia Maesa\n", StandardOpenOption.APPEND);
+//        } catch (IOException ioe){
+//            ioe.printStackTrace();
+//        }
+//
+////        io.readFileAndOuput(toOurDataFile);
+//
+//        // Append a list to a list
+//
+//        List<String> lateRomanEmpresses = new ArrayList<>();
+//        lateRomanEmpresses.add("Galla Placidia");
+//        lateRomanEmpresses.add("Theodora");
+//        lateRomanEmpresses.add("Licinia Eudoxia");
+//
+//        try {
+//            Files.write(toOurDataFile, lateRomanEmpresses, StandardOpenOption.APPEND);
+//        } catch (IOException ioe){
+//            ioe.printStackTrace();
+//        }
+
+//        io.readFileAndOuput(toOurDataFile);
+
+        // Iterate over a list and remove one item
+
+//        List<String> currentList = new ArrayList<>();
+//        try {
+//            currentList = Files.readAllLines(toOurDataFile);
+//        } catch (IOException ioe){
+//            ioe.printStackTrace();
+//        }
+//        Iterator<String> listIterator = currentList.iterator();
+//        while(listIterator.hasNext()){
+//            String empress = listIterator.next();
+//            if (empress.equals("Messaline")){
+//               listIterator.remove();
+//            }
+//        }
+//        try {
+//            Files.write(toOurDataFile, currentList);
+//        } catch (IOException ioe){
+//            ioe.printStackTrace();
+//        }
+
         try {
-            if (Files.notExists(toOurDataFile)) {
-                Files.createFile(toOurDataFile);
-            }
+            Files.writeString(toOurDataFile, "Messalina\n", StandardOpenOption.APPEND);
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
 
-        // Write to a file
-
-        List<String> romanEmpresses = Arrays.asList("Livia", "Agrippina", "Messaline", "Julia Domna");
-
-        try {
-            Files.write(toOurDataFile, romanEmpresses);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-
+        io.readFileAndOuput(toOurDataFile);
     }
 }
