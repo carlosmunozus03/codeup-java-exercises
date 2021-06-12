c89d50cb1af653cea92f9b1d4a823e8ef844169b
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-06-12 16:11:25 -0500

    practice 1 assessment java 1

--- src/HWPractice.java
+++ src/HWPractice.java
@@ -1,5 +1,39 @@
 public class HWPractice {
     public static void main(String[] args) {
         System.out.println("Hello World Practice!");
+        int myFavoriteNumber = 42;
+        System.out.println(myFavoriteNumber);
+        String myString = "This is a practice string for the 1 java assessment";
+        System.out.println(myString);
+        long myNumber;
+        myNumber = 123;
+        System.out.println(myNumber);
+        float myNumberFloat = (float) myNumber;
+        System.out.println(myNumberFloat);
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+        System.out.println("-----");
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+        System.out.println("-----\n");
+        int a = 4;
+        a += 5;
+        System.out.println(a);
+        int b = 3;
+        int c = 4;
+        c *= b;
+        System.out.println(c);
+        int d = 10;
+        int e = 2;
+        d /= e;
+        e -= d;
+        System.out.println(d);
+        System.out.println(e);
+        e = e - d;
+        System.out.println(e);
+        int maxNUmber = Integer.MAX_VALUE;
+        System.out.println(maxNUmber);
     }
 }
